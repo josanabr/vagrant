bf816ad5682e11367505789ec64ad1fef0f4bbdd
josanabr <john.sanabria@correounivalle.edu.co>
2017-05-09 17:48:43 -0500

    fixing htcondor installation

--- HTCondor/script.sh
+++ HTCondor/script.sh
@@ -2,5 +2,9 @@
 wget -qO - http://research.cs.wisc.edu/htcondor/ubuntu/HTCondor-Release.gpg.key | sudo apt-key add -
 sudo echo "deb http://research.cs.wisc.edu/htcondor/ubuntu/stable/ trusty contrib" >> /etc/apt/sources.list
 sudo apt-get update
-#sudo DEBIAN_FRONTEND=noninteractive apt-get -y install htcondor
-
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install htcondor
+sudo echo "start = true" >> /etc/condor/condor_config.local
+sudo service condor restart
+echo "-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-"
+echo "  Para probar la instalacion ejecute 'condor_submit /vagrant/demo.condor'"
+echo "-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-=*=-"
