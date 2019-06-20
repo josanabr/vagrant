800186726d2e5607716ec77f6e0162fb32b0bc41
John Sanabria <john.sanabria@gmail.com>
2019-06-20 16:40:43 -0500

    actualizando instalacion HTCondor

--- HTCondor/script.sh
+++ HTCondor/script.sh
@@ -1,28 +1,7 @@
 #!/usr/bin/env bash
 sudo echo "deb http://research.cs.wisc.edu/htcondor/ubuntu/stable/ trusty contrib" >> /etc/apt/sources.list
-wget -qO - http://research.cs.wisc.edu/htcondor/ubuntu/HTCondor-Release.gpg.key | sudo apt-key add -
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install condor docker.io
-sudo -i
-cat << EOT >> /etc/condor/condor_config.local
-
-QUEUE_ALL_USERS_TRUSTED = TRUE
-ALLOW_READ = *
-ALLOW_WRITE = *
-HOSTALLOW_READ = *
-HOSTALLOW_WRITE = *
-ALLOW_NEGOTIATOR = *
-ALLOW_ADMINISTRATOR = *
-COLLECTOR_DEBUG = D_FULLDEBUG
-NEGOTIATOR_DEBUG = D_FULLDEBUG
-MATCH_DEBUG = D_FULLDEBUG
-SCHEDD_DEBUG = D_FULLDEBUG
-START = TRUE
-TRUST_UID_DOMAIN = TRUE
-USE_SHARED_PORT=FALSE
-
-EOT
-sudo echo "DOCKER_OPTS=\"--insecure-registry 192.168.28.50:5000\"" >> /etc/default/docker
 sudo groupadd docker
 sudo gpasswd -a ${USER} docker
 sudo gpasswd -a condor docker
