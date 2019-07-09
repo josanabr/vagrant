9a323941f423ad9af89794ea07d247a2362114cb
John Sanabria <john.sanabria@gmail.com>
2019-07-09 18:50:20 -0500

    First commit

---
+++ HTCondor/README.md
@@ -0,0 +1,22 @@
+# HTCondor - All in one
+
+En este repositorio se encuentran los archivos necesarios para hacer el despliegue de Docker + HTCondor (All-in-one). 
+En una máquina virtual se instalará [Docker](https://www.docker.com) y [HTCondor](https://research.cs.wisc.edu/htcondor/) y se tendrá un sistema funcional con estas dos herramientas usadas en entornos HTC.
+
+## Requerimientos
+
+Instalar la herramienta [vagrant](https://vagrantup.com).
+Durante el desarrollo de los scripts en este repositorio se usó la versión 2.2.5 de Vagrant.
+
+---
+
+## Pasos para el despliegue Docker + HTCondor (All-In-One)
+
+Asumiendo que se ha instalado vagrant, desde una terminal ejecutar los siguientes pasos:
+
+[1] `vagrant up`
+[2] `vagrant ssh`
+[3] `condor_submit docker.condor`
+[4] `cat out.0`
+
+
--- HTCondor/script.sh
+++ HTCondor/script.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
-sudo echo "deb http://research.cs.wisc.edu/htcondor/ubuntu/stable/ trusty contrib" >> /etc/apt/sources.list
+#sudo echo "deb http://research.cs.wisc.edu/htcondor/ubuntu/stable/ xenial contrib" >> /etc/apt/sources.list
 sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install condor docker.io
-sudo groupadd docker
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes install condor docker.io
+sudo -i
+cat << EOT >> /etc/condor/condor_config.local
+QUEUE_ALL_USERS_TRUSTED = TRUE
+CONDOR_HOST = \$(FULL_HOSTNAME)
+ALLOW_READ = *
+ALLOW_WRITE = *
+HOSTALLOW_READ = *
+HOSTALLOW_WRITE = *
+ALLOW_NEGOTIATOR = *
+ALLOW_ADMINISTRATOR = *
+COLLECTOR_DEBUG = D_FULLDEBUG
+NEGOTIATOR_DEBUG = D_FULLDEBUG
+MATCH_DEBUG = D_FULLDEBUG
+SCHEDD_DEBUG = D_FULLDEBUG
+START = TRUE
+TRUST_UID_DOMAIN = TRUE
+USE_SHARED_PORT=FALSE
+NETWORK_INTERFACE = 127.0.0.1
+DAEMON_LIST=MASTER,COLLECTOR,NEGOTIATOR,SCHEDD,STARTD
+EOT
 sudo gpasswd -a ${USER} docker
 sudo gpasswd -a condor docker
 sudo gpasswd -a vagrant docker
