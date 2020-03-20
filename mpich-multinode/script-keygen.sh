260102b6ceadf810f9a777bda008078aab4b205f
John Sanabria <johnsanabria@correounivalle.edu.co>
2020-03-19 23:17:52 -0500

    modifygin script-keygen

--- mpich-multinode/script-keygen.sh
+++ mpich-multinode/script-keygen.sh
@@ -5,4 +5,5 @@
 # Author: John Sanabria 
 # Date: 2020-03-19
 #
-ssh-keygen -t rsa -q -f "$HOME/.ssh/id_rsa" -N ""
+ssh-keygen -t rsa -q -f "/home/vagrant/.ssh/id_rsa" -N ""
+chown -R vagrant:vagrant /home/vagrant/.ssh
